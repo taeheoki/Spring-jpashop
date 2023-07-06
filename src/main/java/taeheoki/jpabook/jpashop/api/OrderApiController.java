dba7058ce0b3c31c70d5f67e59565a8c6c275845
taeheoki <dkqjwl2365@naver.com>
2023-07-06 23:53:29 +0900

    🧑🏻‍💻feat: 컬렉션 조회 최적화 V3.1, V4, V5, V6

--- src/main/java/taeheoki/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/taeheoki/jpabook/jpashop/api/OrderApiController.java
@@ -3,6 +3,7 @@ package taeheoki.jpabook.jpashop.api;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import taeheoki.jpabook.jpashop.domain.Address;
 import taeheoki.jpabook.jpashop.domain.Order;
@@ -10,10 +11,17 @@ import taeheoki.jpabook.jpashop.domain.OrderItem;
 import taeheoki.jpabook.jpashop.domain.OrderStatus;
 import taeheoki.jpabook.jpashop.repository.OrderRepository;
 import taeheoki.jpabook.jpashop.repository.OrderSearch;
+import taeheoki.jpabook.jpashop.repository.order.query.OrderFlatDto;
+import taeheoki.jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import taeheoki.jpabook.jpashop.repository.order.query.OrderQueryDto;
+import taeheoki.jpabook.jpashop.repository.order.query.OrderQueryRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
 
 /**
  * V1. 엔티티 직접 노출
@@ -40,6 +48,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     /**
      * V1. 엔티티 노출
@@ -68,6 +77,50 @@ public class OrderApiController {
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
         return orders.stream().map(OrderDto::new).collect(Collectors.toList());
+    }
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(),
+                                o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())))
+                .entrySet()
+                .stream().map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(),
+                        e.getKey().getOrderDate(),
+                        e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(),
+                        e.getValue()))
+                .collect(toList());
     }
 
     @Data
